d2cb9f2316d70593a67480f240e2cf600a9bf64a
MrTryAgain <mrtryagain2021@gmail.com>
2021-07-18 21:49:20 +0200

    erster commit vom Working Tree

--- php-pdo-opp-main/src/footerX.php
+++ php-pdo-opp-main/src/footerX.php
@@ -2,7 +2,7 @@
 
     <div class="footer">
         <h2>
-            <p>Dies ist mein Footer, ganz simpel aber ich bin auch nur eine einfache Website</p>
+            <p>Dies ist mein Footer, ganz simpel aber ich bin auch nur eine einfache Quiz_-Site ;)</p>
         </h2>
     </div>
 </footer>
